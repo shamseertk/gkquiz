e97a551de1beb3ac8cebcc15c38a7e6a2415d849
Shamseer <shamseertk@gmail.com>
2022-04-10 05:39:34 -0700

    updated the code to more than 4

--- src/container/DumbCharade.js
+++ src/container/DumbCharade.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import { generateRandomNumber } from '../utils/utils';
 import { collection, getDocs } from "firebase/firestore/lite";
 import { db } from '../services/fb';
+import _ from 'lodash';
 
 function DumbCharade() {
   const [dcLocalData, setDCLocalData] = useState([]);
@@ -27,7 +28,7 @@ function DumbCharade() {
   const [currPlayer, setCurrPlayer] = useState('');
 
   useEffect(() => {
-    if(parseInt(numPlayers) === 3) {
+    if(parseInt(numPlayers) >= 3) {
       const nPL = Array.from({length: numPlayers}).map((_, pl) => ({
           value: '',
       }));
@@ -35,7 +36,7 @@ function DumbCharade() {
     } else {
       setPlayers([]);
     }
-  }, [numPlayers, setPlayers]);
+  }, [numPlayers]);
   
   const handlePlayerName = (evt, index) => {
     const playerName = evt.target.value;
@@ -44,27 +45,53 @@ function DumbCharade() {
     setPlayers(playerUpd);
   };
 
+  const getTheFirstTeamIndex = (checkArrExi) => {
+    const randIndex = generateRandomNumber(players.length);
+    if (checkArrExi.indexOf(randIndex) > -1) {
+      return getTheFirstTeamIndex(checkArrExi);
+    } else {
+      return randIndex;
+    }
+  }
+
   const handleGenerateTeam = () => {
-    setTeam([{
-      from: players[0].value,
-      to: players[1].value,
-    }, {
-      from: players[0].value,
-      to: players[2].value,
-    }, {
-      from: players[1].value,
-      to: players[2].value,
-    }, {
-      from: players[1].value,
-      to: players[0].value,
-    }, {
-      from: players[2].value,
-      to: players[0].value,
-    }, {
-      from: players[2].value,
-      to: players[1].value,
-    }]);
-    setCurrPlayer(`${players[0].value}${players[1].value}`);
+    if(players.length === 3) {
+      setTeam([{
+        from: players[0].value,
+        to: players[1].value,
+      }, {
+        from: players[0].value,
+        to: players[2].value,
+      }, {
+        from: players[1].value,
+        to: players[2].value,
+      }, {
+        from: players[1].value,
+        to: players[0].value,
+      }, {
+        from: players[2].value,
+        to: players[0].value,
+      }, {
+        from: players[2].value,
+        to: players[1].value,
+      }]);
+      setCurrPlayer(`${players[0].value}${players[1].value}`);
+    } else {
+      const teamAIndexes = [];
+      Array.from({length: Math.ceil(parseInt(players.length)/2)}).forEach((_ , ind) => {
+        const randIndex = getTheFirstTeamIndex(teamAIndexes);
+        teamAIndexes.push(randIndex);
+      });
+      const team = [];
+      teamAIndexes.forEach(ind => {
+        players.forEach((pl, plIndex) => {
+          if(teamAIndexes.indexOf(plIndex) === -1){
+            team.push({from: players[ind].value, to: pl.value})
+          }
+        })
+      })
+      setTeam(team);
+    }
   };
 
   const handleGetRandomAnimal = (from, to) => {
@@ -77,19 +104,65 @@ function DumbCharade() {
     }
   };
 
+  const getTheTeams = () => {
+    const teamA = Object.keys(_.groupBy(team, item => item.from));
+    const teamB = Object.keys(_.groupBy(team, item => item.to));
+    return {
+      wholeTeam: _.compact(_.flatMap(_.zip(teamA, teamB))),
+      teamA,
+      teamB
+    };
+  }
+
   return <>
-  <FormControl>
-    <FormLabel>How many Players? 
-      <TextField type="number" value={numPlayers} onChange={(event) => setNumPlayers(event.target.value)} />
-    </FormLabel>
-      {players && players.map((pl, ind) => 
-    <FormLabel key={`${ind}-Playername`}>{ind + 1}. Name: <TextField
-      value={pl.value}
-      onChange={(event) => handlePlayerName(event, ind)} /></FormLabel>)}
-    {parseInt(numPlayers) === 3 && <Button onClick={handleGenerateTeam}>Generate The Team</Button>}
-    {parseInt(numPlayers) > 3 && <Button onClick={handleGetRandomAnimal}>Start</Button>}
-  </FormControl>
+    <Grid container>
+      <Grid item md={12}>
+        <FormLabel>How many Players? 
+          <TextField type="number" value={numPlayers} onChange={(event) => setNumPlayers(event.target.value)} />
+        </FormLabel>
+      </Grid>
+      {players && players.length >= 3 && players.map((pl, ind) => <Grid item md={4}>{ind + 1}. Name: <TextField
+            value={pl.value}
+            onChange={(event) => handlePlayerName(event, ind)} /></Grid>)}
+    </Grid>
+    {parseInt(numPlayers) >= 3 && <Button onClick={handleGenerateTeam}>Generate The Team</Button>}
   <Grid container>
+    <Grid item md={4}>
+      <List component="nav">
+        {parseInt(numPlayers) === 3 ? team.map(tm => <> <ListItem disabled={dcLocalData.length < 1} key={`${tm.from}${tm.to}`} button className="list-item" onClick={() => handleGetRandomAnimal(tm.from, tm.to)}>
+          <ListItemText className={`${tm.from}${tm.to}` === currPlayer ? `list-item-selected` : `list-item`} primary={`${tm.from}  ---> ${tm.to}`} />
+        </ListItem>
+        <ListItem disabled={dcLocalData.length < 1} key={`${tm.to}${tm.from}`} button className="list-item" onClick={() => handleGetRandomAnimal(tm.to, tm.from)}>
+          <ListItemText className={`${tm.to}${tm.from}` === currPlayer ? `list-item-selected` : `list-item`} primary={`${tm.to}  ---> ${tm.from}`} />
+        </ListItem></>)
+        : getTheTeams().wholeTeam.map(pl =><ListItem disabled={dcLocalData.length < 1} key={`${pl}`} button className="list-item" onClick={() => handleGetRandomAnimal(pl)}>
+        <ListItemText className={`${pl}` === currPlayer ? `list-item-selected` : `list-item`} primary={`${pl}${getTheTeams().teamA.indexOf(pl) > -1 ? '(TeamA)' : '(TeamB)'}`} />
+      </ListItem>)}
+      </List>
+    </Grid>
+    <Grid item md={8}>
+      {currentDCData.name}
+    </Grid>
+  </Grid>
+ {/*  <Grid container>
+    <Grid item md={12}>
+        <FormLabel>How many Players? 
+          <TextField type="number" value={numPlayers} onChange={(event) => setNumPlayers(event.target.value)} />
+        </FormLabel>
+    </Grid>
+          {players && players.map((pl, ind) => <Grid item md={4}>
+          <FormLabel key={`${ind}-Playername`}>{ind + 1}. Name: <TextField
+            value={pl.value}
+            onChange={(event) => handlePlayerName(event, ind)} /></FormLabel>
+            </Grid>)}
+            {parseInt(numPlayers) === 3 && <Button onClick={handleGenerateTeam}>Generate The Team</Button>}
+            {parseInt(numPlayers) > 3 && <Button onClick={handleGetRandomAnimal}>Start</Button>}
+    <Grid item md={6}>
+      {players.splice(0, Math.floor(players.length/2)).map((pl ,i) => <div>{i}. Test</div>)}
+    </Grid>
+    <Grid item md={6}>
+      testing
+    </Grid>
     <Grid item md={4}>
       <List component="nav">
         {team.map(tm => <ListItem disabled={dcLocalData.length < 1} key={`${tm.from}${tm.to}`} button className="list-item" onClick={() => handleGetRandomAnimal(tm.from, tm.to)}>
@@ -100,7 +173,7 @@ function DumbCharade() {
     <Grid item md={8}>
       {currentDCData.name}
     </Grid>
-  </Grid>
+  </Grid> */}
   </>
 }
 
