a311a2cdb2121d3077f260642d0303d10f80ee07
Shamseer <shamseertk@gmail.com>
2022-11-16 19:51:30 -0700

    added more words

--- src/container/SpellingBee.js
+++ src/container/SpellingBee.js
@@ -1,15 +1,16 @@
-
 import { Box } from "@mui/material";
 import PageTitle from "../components/common/PageTitle";
 import { threeg } from "../data/spelling";
 
 function SpellingBee() {
+
   return <div>
     <PageTitle title="Spelling Bee" />
     <Box>
       {threeg && threeg.map(word => <div>
-        <audio jsname="QInZvb" preload="auto" controls> 
+        <audio jsname="QInZvb" preload="auto" controls>
         <source src={`https://ssl.gstatic.com/dictionary/static/sounds/20200429/${word}--_us_1.mp3`} /> </audio>
+        
         <a target="_blank"rel="noreferrer" href={`https://www.google.com/search?q=${word}+definition`} title="">{word}</a>
         </div>
 
